66c657123743cf04ccf13afe9a82ee91870f5364
gabriele Demasi <gabriele.demasi@unifi.it>
2025-08-25 16:41:42 +0200

    migrating functions

---
+++ smc_functions.py
@@ -0,0 +1,44 @@
+import jax
+from utils import compute_mass_matrix
+import jax.numpy as jnp 
+
+
+def build_mass_matrix_fn(log_posterior):
+    def single(pos, beta):
+        logdensity = lambda x: log_posterior(x, beta)
+        
+        return compute_mass_matrix(logdensity, pos)
+    return jax.jit(jax.vmap(single, in_axes=(0, None)))
+
+
+
+def make_smc_step_fn(init_fn, kernel_fn,log_posterior):
+    def make_a_step_vectorized(position, keys, beta ,matrices):
+        """
+        position: (M, D)
+        beta: scalar or vector
+        """
+        logdensity_fn = lambda x: log_posterior(x, beta)  # Only for init, not passed into JIT
+
+        # Initialize state
+        state = init_fn(position, logdensity_fn,)
+
+        beta_batch = jnp.broadcast_to(beta, (position.shape[0],))
+
+        state, info = kernel_fn(keys, state,  beta_batch, matrices)
+
+        return state.position
+    
+    return make_a_step_vectorized
+
+
+def build_kernel_fn(kernel, log_posterior, step_size):
+    def _kernel(rng_key, state, beta, metric):
+        logdensity_fn = lambda x: log_posterior(x, beta)
+        return kernel(rng_key, state, logdensity_fn, step_size, metric)
+
+    # JIT-compile the batched kernel function
+    batched_kernel = jax.jit(jax.vmap(_kernel, in_axes=(0, 0, 0, 0), out_axes=(0, 0)))
+    return batched_kernel
+
+
