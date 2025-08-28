fixed evidence computation with bootstraping; seems to work on gaussians

smc.py
@@ -39,25 +39,24 @@ import json
 
 from jax.scipy.special import logsumexp
  
-def prior(params):
-    return  0.0 -100 # Uniform prior within bounds, log(1) = 0
 
-def log_likelihood_bimodal(params):
-    mean1 = jnp.array([-0.0, -0.0])
-    mean2 = jnp.array([1.0, 1.0])
-    
-    cov  = jnp.array([[1.0, 0.], [0., 1.0]])
 
-    inv_cov = jnp.linalg.inv(cov)
+# def log_likelihood(params):
+#     mean1 = jnp.array([-1.0, -1.0])
+#     mean2 = jnp.array([1.0, 1.0])
     
-    diff1 = params - mean1
-    diff2 = params - mean2
-    exponent1 = -0.5 * jnp.einsum('...i,ij,...j->...', diff1, inv_cov, diff1)
-    exponent2 = -0.5 * jnp.einsum('...i,ij,...j->...', diff2, inv_cov, diff2)
-    norm_const = -0.5 * jnp.log(jnp.linalg.det(2 * jnp.pi * cov))
-    logpdf1 = exponent1 + norm_const
-    logpdf2 = exponent2 + norm_const
-    return logsumexp(jnp.array([logpdf1, logpdf2]))
+#     cov  = jnp.array([[0.01, 0.], [0., 0.01]])
+
+#     inv_cov = jnp.linalg.inv(cov)
+    
+#     diff1 = params - mean1
+#     diff2 = params - mean2
+#     exponent1 = -0.5 * jnp.einsum('...i,ij,...j->...', diff1, inv_cov, diff1)
+#     exponent2 = -0.5 * jnp.einsum('...i,ij,...j->...', diff2, inv_cov, diff2)
+#     norm_const = -0.5 * jnp.log(jnp.linalg.det(2 * jnp.pi * cov))
+#     logpdf1 = exponent1 + norm_const
+#     logpdf2 = exponent2 + norm_const
+#     return logsumexp(jnp.array([logpdf1, logpdf2]))
 
 def log_likelihood(params):
     mean = jnp.array([0.0, 0.0])
@@ -66,14 +65,14 @@ def log_likelihood(params):
     diff = params - mean
     exponent = -0.5 * jnp.einsum('...i,ij,...j->...', diff, inv_cov, diff)
     norm_const = -0.5 * jnp.log(jnp.linalg.det(2 * jnp.pi * cov))
-    return exponent + norm_const
+    return exponent  + norm_const
 
 
 
 
 
 def prior(params):
-    return  0. # Uniform prior within bounds, log(1) = 0
+    return 0. # Uniform prior within bounds, log(1) = 0
 
 
 
@@ -84,7 +83,7 @@ def log_posterior(params, beta=1):
 
 prior_bounds            = jnp.array([[-5, 5], [-5, 5]])
 boundary_conditions     = jnp.array([0, 0])  # 0: periodic, 1: reflective
-number_of_particles     = 3000
+number_of_particles     = 1000
 step_size               = 1e-2
 temperature_schedule    = jnp.logspace(-2, 0, 20)
 temperature_schedule    = temperature_schedule[1:]

smc_functions.py
@@ -72,7 +72,7 @@ def compute_weight_and_ess_fn(log_posterior):
         beta = beta_after - beta_before
         log_weights = jax.vmap(log_density_diff, in_axes=(0, None))(samples, beta)
         
-        log_weights = log_weights - jnp.max(log_weights)
+        log_weights = log_weights #- jnp.max(log_weights)
         weights_nonorm = jnp.exp(log_weights)
         weights = weights_nonorm / jnp.sum(weights_nonorm)
         ess = (jnp.sum(weights)) ** 2 / jnp.sum(weights**2)
@@ -98,6 +98,7 @@ def smc_step_fn(mass_matrix_fn, mutation_step_vectorized, compute_weight_and_ess
         samples         = multinomial_resample(resampling_key, samples, weights)
         # Mutation
         matrices        = mass_matrix_fn(samples, beta)
+        
         samples         = mutation_step_vectorized(samples, mutation_keys, beta, matrices)
 
         return samples, weights, weights_nonorm, ess
@@ -159,7 +160,7 @@ def run_smc(log_posterior, prior_bounds, boundary_conditions, temperature_schedu
 
         samples, weights_nonorm, weights, ess   = step_for(samples, beta, beta_prev,weights, resampling_key, mutation_key)
         samples_dict[step]["samples"]           = np.array(samples).tolist()
-        samples_dict[step]["weights"]           = np.array(weights_nonorm).tolist()
+        samples_dict[step]["weights"]           = np.array(weights).tolist()
         samples_dict[step]["ess"]               = float(ess)
 
         print("ess = {}".format(ess))
@@ -178,16 +179,19 @@ def compute_evidence(result_path):
     
 
     for key in result.keys():
-        if float(key) > 1:
+        if float(key) > -1:
             
             evidence_piece = np.sum(result[key]['weights'])/len(result[key]['weights'])
             
             
             evidence      *= evidence_piece
 
-            print(np.log(evidence_piece))
-            error_piece    = np.var(result[key]['weights'])/(np.sum(result[key]['weights'])/len(result[key]['weights']))**2/len(result[key]['weights'])
-            error      += error_piece
+            ### compute evidence with bootstraping
+            boot_weights = np.array(result[key]['weights'])
+            
+            dlogz_piece = np.var([np.sum(boot_weights[np.random.choice(len(boot_weights), len(boot_weights))])/len(boot_weights) for _ in range(1000)])
+            
+            error += dlogz_piece 
             
     return np.log(evidence), np.sqrt(error)
 